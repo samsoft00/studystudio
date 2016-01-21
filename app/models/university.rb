d5ab0c15f991c81c59decefac6f4ea5a715df238
samsoft00 <oyewoleabayomi@gmail.com>
2016-01-21 13:29:42 +0100

    first commit, enable multitenancy and authentication

---
+++ app/models/university.rb
@@ -0,0 +1,15 @@
+class University < ActiveRecord::Base
+	after_create :create_tenant
+
+	validates :subdomain, exclusion: { in: %w(www admin us ca jp), message: "%{value} is reserved." }
+	validates :subdomain, length: { in: 6..20 }
+	validates :subdomain, uniqueness: true
+	validates :name, :site, :logo, :portal, :subdomain, presence: true
+
+	private
+
+	def create_tenant
+		Apartment::Tenant.create(subdomain)
+	end
+
+end
---
+++ db/migrate/20160121085853_create_universities.rb
@@ -0,0 +1,13 @@
+class CreateUniversities < ActiveRecord::Migration
+  def change
+    create_table :universities do |t|
+      t.string :name
+      t.string :site
+      t.string :logo
+      t.string :portal
+      t.string :subdomain
+
+      t.timestamps null: false
+    end
+  end
+end
